5710219b29093d0f11aef122512fe5231f2e9fa7
Mohammed <mohammed.hussein.mahmoud@gmail.com>
2019-01-22 13:41:18 +0200

    commit your first project

---
+++ Library.java
@@ -0,0 +1,11 @@
+/**
+ * starting 22/1/2019
+ * first step in project Silicate...
+ */
+
+ /** */
+class Library {
+    private class Reader{}
+    private class Writer{}
+    private class Interpreter{}
+}
